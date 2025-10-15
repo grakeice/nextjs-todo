c1eae5336ca32e79b7117f533bcadf37fa7fdd43
grakeice <89824152+grakeice@users.noreply.github.com>
2025-10-15 18:36:12 +0900

    initial commit

---
+++ apps/frontend/eslint.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	{
+		ignores: [
+			"node_modules/**",
+			".next/**",
+			"out/**",
+			"build/**",
+			"next-env.d.ts",
+		],
+	},
+	eslintConfigPrettier,
+];
+
+export default eslintConfig;
