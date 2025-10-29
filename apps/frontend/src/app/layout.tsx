2568128c6501891a973f70772aa20e6dd9620d29
grakeice <89824152+grakeice@users.noreply.github.com>
2025-10-29 19:04:15 +0900

    公式ドキュメントの導くままにApolloWrapperを作成
    https://github.com/apollographql/apollo-client-integrations/tree/main/packages/nextjs

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
+import { ApolloWrapper } from "@/graphql/ApolloWrapper";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +30,7 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				{children}
+				<ApolloWrapper>{children}</ApolloWrapper>
 			</body>
 		</html>
 	);
